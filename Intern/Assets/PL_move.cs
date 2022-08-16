edc0639863b888fe4ab62358a8d06ff08dab61ce
kuwahar <d58041@gm.kochi-ct.jp>
2022-08-16 16:32:42 +0900

    PL向き変更実装

--- Intern/Assets/PL_move.cs
+++ Intern/Assets/PL_move.cs
@@ -9,11 +9,12 @@ public class PL_move : MonoBehaviour
     private float mv_speed;
     private bool isGround;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        j_speed = (float)5;
-        mv_speed = (float)3;
+        j_speed = 5;
+        mv_speed = 3;
         rb = GetComponent<Rigidbody>();
         isGround = true;
     }
@@ -21,21 +22,24 @@ public class PL_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         rb.velocity = new Vector3(0, rb.velocity.y, 0);
-        if (Input.GetButton("Vertical"))
-        {
-            Move(Input.GetAxis("Vertical"), "z");      
+        if ((Input.GetButton("Vertical")) ^ (Input.GetButton("Horizontal"))) {
+            if (Input.GetButton("Vertical"))
+            {
+                Move(Input.GetAxis("Vertical"), "z");
+            }
+            if (Input.GetButton("Horizontal"))
+            {
+                Move(Input.GetAxis("Horizontal"), "x");
+            }
+            
         }
-        if (Input.GetButton("Horizontal"))
-        {
-            Move(Input.GetAxis("Horizontal"), "x");
-        }
-        if (Input.GetButton("Horizontal") && Input.GetButton("Horizontal"))
+        if (Input.GetButton("Horizontal") && Input.GetButton("Vertical"))
         {
             Move(Input.GetAxis("Horizontal"), "xz", Input.GetAxis("Vertical"));
 
         }
+
         if (Input.GetKeyDown("space") && isGround)
         {
             Jump();
@@ -45,19 +49,29 @@ public class PL_move : MonoBehaviour
 
     void Move(float vec, string axis, float vec2 = 0) 
     {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        // ローカル座標を基準に、回転を取得
+        Vector3 worldAngle = myTransform.localEulerAngles;
+
         if (axis.Equals("z"))
         {
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, vec * mv_speed);
+            worldAngle.y = vec*180.0f; // ローカル座標を基準に、z軸を軸にした回転を10度に変更
         }
         if (axis.Equals("x"))
         {
             rb.velocity = new Vector3(vec * mv_speed, rb.velocity.y, rb.velocity.z);
+            worldAngle.y = vec*90.0f; // ローカル座標を基準に、x軸を軸にした回転を10度に変更
         }
         if (axis.Equals("xz"))
         {
             rb.velocity = new Vector3(vec * mv_speed, rb.velocity.y, vec2 * mv_speed);
-        }
+            worldAngle.y = (vec*vec2) * 45.0f; // ローカル座標を基準に、x軸を軸にした回転を10度に変更
 
+        }
+        myTransform.localEulerAngles = worldAngle; // 回転角度を設定
     }
 
     void Jump()
