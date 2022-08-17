bf45d1ba47511f583fbb0f21d19b1a2bae2aca6b
kuwahar <d58041@gm.kochi-ct.jp>
2022-08-17 11:33:26 +0900

    block設置処理 途中

--- Intern/Assets/Pickup_follow.cs
+++ Intern/Assets/Pickup_follow.cs
@@ -10,36 +10,62 @@ public class Pickup_follow : MonoBehaviour
     private Vector3 posB;
     private Vector3 posP;
     private bool isHold = false;
+    private float rayDistance;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rayDistance = 2.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        var direction = transform.forward;
+        Vector3 rayPosition = transform.position + new Vector3(0.0f, -2.0f, 0.0f);
+        Ray ray = new Ray(rayPosition, direction);
+        Debug.DrawRay(rayPosition, direction * rayDistance, Color.red);
+
         posB = this.transform.position;
         posP = Player.transform.position;
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            if (hit.collider.name == "Ground")
+            {
+                Debug.Log("Hit");
+            }
+        }
+
 
         if (Dist(posB, posP))
         //if (Dist(posB, posP) && isHold)
         {
-            if (Input.GetKeyDown("space"))
+            if (Input.GetKeyUp("e"))
                 isHold = true;
         }
         if (isHold)
         {
-            this.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + 1, Player.transform.position.z);
+
+            this.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + 1.1f, Player.transform.position.z);
+
+            if (Input.GetKeyDown("e"))
+            {
+                //ブロック設置処理
+
+
+                isHold = false;
+
+            }
         }
-        else Debug.Log("TEST");
     }
 
     bool Dist(Vector3 pos1, Vector3 pos2)
     {
         dis = Vector3.Distance(pos1, pos2);
-        if (dis < 2)
+        if (dis < 1.2f)
         {
             return true;
         }
