62fb43525a5999b7c869c705ade18e445cb39afa
MBPC-00429\monoai-kochi-01 <d58003@gm.kochi-ct.jp>
2022-08-17 15:58:22 +0900

    使用アセットの導入とメインキャラに移動を適用

---
+++ Intern/Assets/fade.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class fade : MonoBehaviour
+{
+    //public Gameobject image;
+
+    void Start()
+    {
+        /*image = GetComponent<Image>();
+        image.enabled = true; 
+        image.color = new Color(255, 0, 0, 0.5f); */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
