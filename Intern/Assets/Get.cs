1ea267df5b1de41bd16bec32382eb328b28d127a
kuwahar <d58041@gm.kochi-ct.jp>
2022-08-18 16:47:50 +0900

    競合修正およびバグ修正

--- Intern/Assets/Get.cs
+++ Intern/Assets/Get.cs
@@ -2,36 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-<<<<<<< HEAD
-=======
 using System.Threading.Tasks;
->>>>>>> feature_kuwahara/add_star
 
 
 
 public class Get : MonoBehaviour
 {
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
->>>>>>> develop
-    AnimationScript test;
->>>>>>> feature_kuwahara/add_star
+    AnimationScript test = null;
     public bool isStarGot = false;
     void Start()
     {
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
->>>>>>> develop
         test = GetComponent<AnimationScript>();
->>>>>>> feature_kuwahara/add_star
-
-
     }
 
     // Update is called once per frame
@@ -39,31 +20,15 @@ public class Get : MonoBehaviour
     {
         if (isStarGot)
         {
-<<<<<<< HEAD
-            var test = GetComponent<AnimationScript>();
-
-=======
-<<<<<<< HEAD
-            var test = GetComponent<AnimationScript>();
-            test.floatSpeed = 10;
-
-            Invoke("SceneChange", 0.5f);
-        }
-    }
-    void SceneChange()
-    {
-        SceneManager.LoadScene("result");
-    }
-=======
->>>>>>> develop
+            isStarGot = false;
+            test = GetComponent<AnimationScript>();
             GetStar();
-            
-            //Invoke("SceneChange", 1.00f);
         }
     }
 
     private async void GetStar()
     {
+        test.isFloating = true;
         test.floatSpeed = 0.01f;
         await Task.Delay(ctime(1));Å@//ÇQïbíxâÑ
         GetComponent<BoxCollider>().isTrigger = true;
@@ -75,18 +40,11 @@ public class Get : MonoBehaviour
 
     void SceneChange()
     {
-        //transform.position = 
         SceneManager.LoadScene("result");
     }
     private int ctime(float time) //ïbÇ…ïœä∑
     {
         time *= 1000;
         return (int)time;
-
-<<<<<<< HEAD
-    }}
-=======
     }
->>>>>>> feature_kuwahara/add_star
 }
->>>>>>> develop
