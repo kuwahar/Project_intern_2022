ede86103b6d6ad9130e67f0ca67a6371f020807f
kuwahar <d58041@gm.kochi-ct.jp>
2022-08-18 16:21:00 +0900

    starゲット完成

--- Intern/Assets/Get.cs
+++ Intern/Assets/Get.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
 
 
 public class Get : MonoBehaviour
 {
+    AnimationScript test;
     public bool isStarGot = false;
     // Start is called before the first frame update
     void Start()
     {
+        test = GetComponent<AnimationScript>();
 
     }
 
@@ -18,14 +21,31 @@ public class Get : MonoBehaviour
     {
         if (isStarGot)
         {
-            var test = GetComponent<AnimationScript>();
-            test.floatSpeed = 10;
-
-            Invoke("SceneChange", 0.5f);
+            GetStar();
+            
+            //Invoke("SceneChange", 1.00f);
         }
     }
+
+    private async void GetStar()
+    {
+        test.floatSpeed = 0.01f;
+        await Task.Delay(ctime(1));Å@//ÇQïbíxâÑ
+        GetComponent<BoxCollider>().isTrigger = true;
+        test.isFloating = false;
+        test.isScaling = true;
+
+    }
+
     void SceneChange()
     {
+        //transform.position = 
         SceneManager.LoadScene("result");
+    }
+    private int ctime(float time) //ïbÇ…ïœä∑
+    {
+        time *= 1000;
+        return (int)time;
+
     }
 }
